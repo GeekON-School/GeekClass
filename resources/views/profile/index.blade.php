80bb6445eb7aa879d522a6c8a03ed11755b84891
roctbb <roctbb@gmail.com>
2017-09-28 01:20:59 +0300

    random avatars

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -6,11 +6,11 @@
         @foreach($users as $user)
 
             <div class="col-md-4 col-sm-6 col-lg-3" style="margin-bottom: 15px;">
-                <div class="card bg-dark text-white" style="height: 220px;">
+                <div class="card bg-dark text-white" style="height: 250px;">
                     <div class="card-header"><a style="color:white;"
                                                 href="{{url('/insider/profile/'.$user->id)}}"><i class="icon ion-person"></i>&nbsp;{{$user->name}}</a></div>
                     <div class="card-body"
-                         style="background-image: url({{url('/media/'.$user->image)}}); background-size: cover;">
+                         style="@if ($user->image!=null) background-image: url({{url('/media/'.$user->image)}}); @else  background-image: url(https://api.adorable.io/avatars/250/{{$user->id}}.png);  @endif background-size: cover;">
 
                         <p class="card-text"><span style="font-size: 15px;" class="badge badge-pill badge-success"><i
                                         class="icon ion-ios-arrow-up"></i> {{$user->rank()->name}}</span><br>@if ($user->is_trainee)
