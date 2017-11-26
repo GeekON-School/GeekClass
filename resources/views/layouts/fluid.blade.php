a50895bffe68bfd270f3557585ec5acf1cf56cab
roctbb <roctbb@gmail.com>
2017-11-26 22:38:05 +0300

    fix archive cards size

--- resources/views/layouts/fluid.blade.php
+++ resources/views/layouts/fluid.blade.php
@@ -35,7 +35,7 @@
     <script src="{{url('/js/linkify.min.js')}}"></script>
     <script src="{{url('/js/linkify-jquery.min.js')}}"></script>
     <link rel="stylesheet"
-          href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.12.0/build/styles/atom-one-light.min.css">
+          href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.12.0/build/styles/dracula.min.css">
     <script src="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.12.0/build/highlight.min.js"></script>
     <script>hljs.initHighlightingOnLoad();</script>
 
