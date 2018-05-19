35858e470a5417f9e38deab64fca461df51a7731
dedSasha <dr.ddb2@yandex.ru>
2018-05-19 15:29:05 +0300

    user_id to tag_id

---
+++ database/migrations/2018_05_19_115809_user_to_tag_col.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserToTagCol extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('EventTags', function (Blueprint $table) {
+            $table->renameColumn('user_id', 'tag_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('EventTags', function (Blueprint $table) {
+            $table->dropColumn('tag_id');
+        });
+    }
+}
