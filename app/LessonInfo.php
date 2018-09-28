035095dc88e1b33fcf07a8de1d836c2134d97b66
roctbb <roctbb@gmail.com>
2018-09-28 23:40:22 +0300

    магазин

--- app/LessonInfo.php
+++ app/LessonInfo.php
@@ -9,6 +9,5 @@ class LessonInfo extends Model
     protected $table = 'lesson_info';
     public $timestamps = false;
     protected $dates = ['start_date'];
-    protected $primaryKey = 'course_id';
 
 }
