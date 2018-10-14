0cad932b10677fce0b03d861162fb4ccffb6e980
roctbb <roctbb@gmail.com>
2018-10-14 15:53:23 +0300

    др

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,19 @@
 
     @if($user->isBirthday())
         <div class="row">
-        <div class="alert alert-info alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                        aria-hidden="true">&times;</span>
-            </button>
-            <img src="https://png.icons8.com/color/50/000000/confetti.png"> <img src="https://png.icons8.com/color/50/000000/confetti.png"> <img src="https://png.icons8.com/color/50/000000/confetti.png"> <h3>С днем рождения!!!</h3> <img src="https://png.icons8.com/color/50/000000/confetti.png"> <img src="https://png.icons8.com/color/50/000000/confetti.png"> <img src="https://png.icons8.com/color/50/000000/confetti.png">
-        </div>
+            <div class="col">
+                <div class="alert alert-info alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                                aria-hidden="true">&times;</span>
+                    </button>
+                    <img src="https://png.icons8.com/color/50/000000/confetti.png"> <img
+                            src="https://png.icons8.com/color/50/000000/confetti.png"> <img
+                            src="https://png.icons8.com/color/50/000000/confetti.png">
+                    <h3>С днем рождения!!!</h3> <img src="https://png.icons8.com/color/50/000000/confetti.png"> <img
+                            src="https://png.icons8.com/color/50/000000/confetti.png"> <img
+                            src="https://png.icons8.com/color/50/000000/confetti.png">
+                </div>
+            </div>
         </div>
     @endif
 
@@ -43,7 +50,8 @@
         @if ($provider->courses()->where('state', 'started')->count()!=0)
             <div class="row">
                 <div class="col">
-                    <h5 style="font-weight: 300;"><a target="_blank" href="{{$provider->site}}">{{$provider->name}}</a></h5>
+                    <h5 style="font-weight: 300;"><a target="_blank" href="{{$provider->site}}">{{$provider->name}}</a>
+                    </h5>
                 </div>
             </div>
             <div class="row" style="margin-top: 15px;">
@@ -62,18 +70,21 @@
                                     @if ($course->provider->logo != null)
                                         <img src="{{$course->Provider->logo}}" style="margin-top: 15px;height: 50px"/>
                                     @endif
-                                    <h5 style="margin-top: 15px; font-weight: 300;" class="card-title">{{$course->name}}</h5>
+                                    <h5 style="margin-top: 15px; font-weight: 300;"
+                                        class="card-title">{{$course->name}}</h5>
                                     <p class="card-text" style="font-size: 0.8rem;">{{$course->description}}</p>
 
 
                                 </div>
                                 <div class="card-footer" style="background-color: rgba(245,245,245,1);">
                                     @if ($user->role=='teacher' || $course->students->contains($user))
-                                        <a href="{{url('insider/courses/'.$course->id)}}" class="btn btn-success btn-sm"><i
+                                        <a href="{{url('insider/courses/'.$course->id)}}"
+                                           class="btn btn-success btn-sm"><i
                                                     class="icon ion-ios-list-outline"></i>&nbsp;Страница
                                             курса</a>
                                     @else
-                                        <a href="#" class="btn btn-success btn-sm disabled" role="button" aria-disabled="true">Вы
+                                        <a href="#" class="btn btn-success btn-sm disabled" role="button"
+                                           aria-disabled="true">Вы
                                             не
                                             записаны</a>
                                     @endif
@@ -108,7 +119,7 @@
                             <!--<img class="card-img-top" src="..." alt="Card image cap">-->
                             <div class="card-body" style="background-color: rgba(255,255,255,0.9);">
                                 <h4 class="card-title">{{$course->name}}</h4>
-                                <p class="card-text"  style="font-size: 0.8rem;">{{$course->description}}</p>
+                                <p class="card-text" style="font-size: 0.8rem;">{{$course->description}}</p>
 
                             </div>
                             <div class="card-footer" style="background-color: rgba(245,245,245,1);">
@@ -132,10 +143,11 @@
                 @foreach($courses as $course)
                     @if ($course->state == 'ended')
 
-                        <div class="card" style="width: 100%; min-width: 280px; background-image: url({{$course->image}}); background-size: cover;">
+                        <div class="card"
+                             style="width: 100%; min-width: 280px; background-image: url({{$course->image}}); background-size: cover;">
                             <div class="card-body" style="background-color: rgba(255,255,255,0.9);">
                                 <h4 class="card-title">{{$course->name}}</h4>
-                                <p class="card-text"  style="font-size: 0.8rem;">{{$course->description}}</p>
+                                <p class="card-text" style="font-size: 0.8rem;">{{$course->description}}</p>
 
                             </div>
                             <div class="card-footer" style="background-color: rgba(245,245,245,1);">
