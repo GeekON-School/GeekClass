cbaac1e676f711755e6c320573d59ddad7593d6f
roctbb <roctbb@gmail.com>
2019-02-09 21:39:19 +0300

    fix migrations errors

--- database/migrations/2018_08_02_190742_add_course_lesson_info.php
+++ database/migrations/2018_08_02_190742_add_course_lesson_info.php
@@ -14,6 +14,7 @@ class AddCourseLessonInfo extends Migration
     public function up()
     {
         Schema::create('lesson_info', function (Blueprint $table) {
+            $table->increments('id');
             $table->integer('course_id')->unsigned()->nullable();
             $table->foreign('course_id')->references('id')
                 ->on('courses')->onDelete('cascade');
@@ -23,6 +24,7 @@ class AddCourseLessonInfo extends Migration
                 ->on('lessons')->onDelete('cascade');
 
             $table->date('start_date');
+
         });
     }
 
