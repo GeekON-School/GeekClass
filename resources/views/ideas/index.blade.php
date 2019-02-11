042c33086466ede97b5cd5d89b4d320885e1e46c
roctbb <roctbb@gmail.com>
2019-02-11 16:51:55 +0300

    Обновил раздел идеи - добавил модерацию идей

--- resources/views/ideas/index.blade.php
+++ resources/views/ideas/index.blade.php
@@ -4,10 +4,6 @@
     <div class="row" style="margin-top: 15px;">
         <div class="col">
             <h2>Идеи</h2>
-
-            <p style="margin-top: 10px;">В процессе изучения очень важно найти для себя интересные задачи. В этом разделе мы собираем идеи для проектов разной сложности, которые можно реализовать в процессе обучения: просто для развлечения или применять в повседневной жизни.</p>
-            <p>Если у вас своя идея, которую вы хотели бы реализовать или предложить другим участникам - смело нажимайте кнопку <strong>"Добавить"</strong>! Как только ваша идея пройдет проверкуб она появится в этом списке, а вы получите за нее 5 GC.</p>
-
         </div>
         <div class="col">
             <ul class="nav nav-pills float-right" id="ideasTabs" role="tablist">
@@ -28,57 +24,74 @@
     </div>
     <div class="tab-content" id="ideas" style="margin-top: 15px;">
         <div class="tab-pane fade show active" id="active" role="tabpanel" aria-labelledby="active">
-            @foreach($approved_ideas as $letter => $list)
-                <div class="col-12 col-md-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">{{$letter}} </h5>
-                            <ul>
+            <p style="margin-top: 10px;">В процессе изучения очень важно найти для себя интересные задачи. В этом
+                разделе мы собираем идеи для проектов разной сложности, которые можно реализовать в процессе обучения:
+                просто для развлечения или применять в повседневной жизни.</p>
+            <p>Если у вас своя идея, которую вы хотели бы реализовать или предложить другим участникам - смело нажимайте
+                кнопку <strong>"Добавить"</strong>! Как только ваша идея пройдет проверкуб она появится в этом списке, а
+                вы получите за нее 5 GC.</p>
+            <div class="row">
+                @foreach($approved_ideas as $letter => $list)
+                    <div class="col-12 col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">{{$letter}} </h5>
+                                <ul>
 
-                                @foreach($list as $idea)
-                                    <li><a href="{{url('/insider/ideas/'.$idea->id)}}">{{$idea->name}}</a>&nbsp;<img
-                                                style="height: 25px;"
-                                                src="https://img.icons8.com/color/48/000000/idea-sharing.png">
-                                        @if($idea->author->role=='student') &nbsp;<img style="height: 25px;"
-                                                                                       src="https://img.icons8.com/color/48/000000/teamwork.png"
-                                                                                       title="Идея сообщества"> @endif
-                                        <br>
-                                        <span class=" text-muted">{{$idea->short_description}}</span></li>
-                                @endforeach
+                                    @foreach($list as $idea)
+                                        <li><a href="{{url('/insider/ideas/'.$idea->id)}}">{{$idea->name}}</a>&nbsp;<img
+                                                    style="height: 25px;"
+                                                    src="https://img.icons8.com/color/48/000000/idea-sharing.png">
+                                            @if($idea->author->role=='student') &nbsp;<img style="height: 25px;"
+                                                                                           src="https://img.icons8.com/color/48/000000/teamwork.png"
+                                                                                           title="Идея сообщества"> @endif
+                                            <br>
+                                            <span class=" text-muted">{{$idea->short_description}}</span></li>
+                                    @endforeach
 
-                            </ul>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-            @endforeach
-
+                @endforeach
+            </div>
         </div>
         <div class="tab-pane fade" id="draft" role="tabpanel" aria-labelledby="draft">
-            @foreach($draft_ideas as $letter => $list)
-                <div class="col-12 col-md-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">{{$letter}} </h5>
-                            <ul>
+            <p>Здесь вы видите предложенные вами идеи, которые пока не прошли проверку. Если идею долго не проверяют, напомните о ней вашему преподавателю.</p>
+            <div class="row">
+                @if ($draft_ideas->count() == 0)
+                    <div class="jumbotron">
+                        <h1 class="display-4">Настало время для творчества!</h1>
+                        <p class="lead" style="margin-top: 15px;">У тебя наверняка есть идея, которую тебе хотелось бы реализовать (или ты уже сделал в прошлом году и это было круто)? Опиши ее! Тогда твои коллеги тоже смогут разделить радость ее реализации.</p>
+                        <a class="btn btn-primary btn-lg" style="margin-top: 15px;" href="{{url('/insider/ideas/create/')}}" role="button">Предложить идею!</a>
+                    </div>
+                @endif
+                @foreach($draft_ideas as $letter => $list)
+                    <div class="col-12 col-md-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">{{$letter}} </h5>
+                                <ul>
 
-                                @foreach($list as $idea)
-                                    <li><a href="{{url('/insider/ideas/'.$idea->id)}}">{{$idea->name}}</a>&nbsp;<img
-                                                style="height: 25px;"
-                                                src="https://img.icons8.com/color/48/000000/idea-sharing.png">
-                                        @if($idea->author->role=='student') &nbsp;<img style="height: 25px;"
-                                                                                       src="https://img.icons8.com/color/48/000000/teamwork.png"
-                                                                                       title="Идея сообщества"> @endif
-                                        <br>
-                                        <span class=" text-muted">{{$idea->short_description}}</span></li>
-                                @endforeach
+                                    @foreach($list as $idea)
+                                        <li><a href="{{url('/insider/ideas/'.$idea->id)}}">{{$idea->name}}</a>&nbsp;<img
+                                                    style="height: 25px;"
+                                                    src="https://img.icons8.com/color/48/000000/idea-sharing.png">
+                                            @if($idea->author->role=='student') &nbsp;<img style="height: 25px;"
+                                                                                           src="https://img.icons8.com/color/48/000000/teamwork.png"
+                                                                                           title="Идея сообщества"> @endif
+                                            <br>
+                                            <span class=" text-muted">{{$idea->short_description}}</span></li>
+                                    @endforeach
 
-                            </ul>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
 
