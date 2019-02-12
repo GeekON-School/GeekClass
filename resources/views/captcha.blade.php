595a5bae3f37426abc8c32fc7a40a18a8efd3ecf
roctbb <roctbb@gmail.com>
2019-02-12 12:53:10 +0300

    добавил граф в идеи проектов

--- resources/views/captcha.blade.php
+++ resources/views/captcha.blade.php
@@ -1,10 +1,8 @@
-
-
 @if (config('app.enable_recaptcha'))
-  {!! \NoCaptcha::display() !!}
-  @if ($errors->has('g-recaptcha-response'))
-      <span class="help-block">
+    {!! \NoCaptcha::display() !!}
+    @if ($errors->has('g-recaptcha-response'))
+        <span class="help-block">
                           <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
                       </span>
-  @endif
+    @endif
 @endif
