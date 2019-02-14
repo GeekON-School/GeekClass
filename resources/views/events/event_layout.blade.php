300fa88824facce43099a3c91ea72c080047aad8
koishy <artsbyduo@outlook.com>
2019-02-14 12:55:21 +0300

    Students can view only important solutions, or click expand to view all.

---
+++ resources/views/events/event_layout.blade.php
@@ -0,0 +1,25 @@
+<style>
+    .btn
+    {
+        background: none !important;
+        border-color: #ddd !important;
+        color: teal !important;
+        box-shadow: none !important;
+    }
+    .ev
+    {
+        padding: 20px;
+        padding-bottom: 0;
+        border-bottom: 1px solid #ccc;
+    }
+    .btn:hover
+    {
+
+        border-color: rgba(0, 128, 128, 0.1) !important;
+        background: rgba(0, 128, 128, 0.05) !important;
+    }
+    .btn:focus
+    {
+        box-shadow: 0 0 0 3px rgba(0, 128, 128, 0.1) !important;
+    }
+</style>
---
+++ resources/views/events/event_likes_script.blade.php
@@ -0,0 +1,41 @@
+        <script>
+        function dislike(id, el) {
+            el.attr('data-likes', parseInt(el.attr('data-likes')) - 1);
+            var ch = el.children('img').attr('src', 'https://img.icons8.com/material-outlined/24/000000/hearts.png');
+           
+            ch.hide();
+            ch.show(500);
+            $.ajax({
+                url: '/insider/events/'+id+'/dislike_from_events',
+            });
+            el.children('.likes1').html(el.attr('data-likes'));
+
+        }
+        function like(id, el) {
+            el.attr('data-likes', parseInt(el.attr('data-likes')) + 1);
+            
+            var ch = el.children('img').attr('src', 'https://img.icons8.com/material/24/000000/like.png');
+            ch.hide();
+            ch.show(500);
+            $.ajax({
+                url: '/insider/events/'+id+'/like_from_events',
+            });
+
+            el.children('.likes1').html(el.attr('data-likes'));
+
+        }
+        function toggleLike(id, el)
+        {
+            if (el.attr('data-liked') == 'true')
+            {
+                dislike(id, el);
+                el.attr('data-liked', 'false');
+            }
+            else
+            {
+                like(id, el);
+                el.attr('data-liked', 'true');
+            }
+
+        }
+    </script>
